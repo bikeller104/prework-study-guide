babdf3b852b9f6d537320d0ad7e177e8ed9f3334
Brent Keller <bikeller104@gmail.com>
2022-08-14 20:12:21 -0600

    added conditionals and loops to script.js file

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+/*
+var topics = "HTML, CSS, Git, JavaScript";
+console.log(topics) // see if lines need to end with ;
+*/
+var topics = ['HTML','CSS','Git','JavaScript'];
+for(let x = 0; x < topics.length; x++)
+{
+    console.log(/*"Let's study " +*/ topics[x]);
+}
+
+/*
+var topic = "HTML"
+
+if(topic === 'HTML')
+{
+    console.log("Let's study HTML!");
+}
+else if(topic ==='CSS')
+{
+    console.log("Let's study CSS!");
+}
+else if (topic === 'git')
+{
+    console.log("Let's study Git!")
+}
+else if(topic == 'JavaScript')
+{
+    console.log("Let's study JavaScript")   
+}
+else{
+    console.log('Please try again');
+}
+*/
+/*
+//global declaration
+var shapes = ["triangle","square","pentagon", "circle"];
+//output the first item in the array starting at 0
+//console.log(shapes[0]);
+
+for(var x = 0; x < shapes.length; x++)
+{
+    console.log(shapes[x]);
+}
+*/
